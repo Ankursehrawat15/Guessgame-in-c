0dee4d04b03a7a297d4ce2ae5c1414dd1c806c3e
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-08-26 20:44:11 +0530

    user guessing number

--- Guessgame.c
+++ Guessgame.c
@@ -5,3 +5,8 @@ int main(){
     int guess,number,nguesses;
     srand(time(0));
     number=rand()%100+1;
+    do {
+        printf("Guess the number between 1to100\n");
+        scanf("%D",&guess);
+        
+    }
